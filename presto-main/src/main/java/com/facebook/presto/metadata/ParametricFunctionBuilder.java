5b28b235157c1421b455e1b91029218c5f2979b7
Karol Sobczak <Karol.Sobczak@Teradata.com>
2015-08-07 12:37:55 +0200

    Builder for Signature and ParametricFunction
    ParametricFunctionBuilder can be used to define
    scalar parametric function with multiple
    specialization methods. Specialization method
    will be selected based on concrete java type parameters
    and return type. It is also possible to define extra
    parameters that will be passed to specialization method.

---
+++ presto-main/src/main/java/com/facebook/presto/metadata/ParametricFunctionBuilder.java
@@ -0,0 +1,174 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.metadata;
+
+import com.facebook.presto.spi.type.Type;
+import com.google.common.collect.ImmutableList;
+
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.ImmutableList.copyOf;
+import static com.google.common.collect.Iterables.getLast;
+import static com.google.common.collect.Lists.newArrayList;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
+
+public final class ParametricFunctionBuilder
+{
+    private final Class<?> clazz;
+    private Signature signature;
+    private String description;
+    private boolean hidden;
+    private boolean deterministic;
+    private boolean nullableResult;
+    private List<Boolean> nullableArguments = emptyList();
+    private List<MethodsWithExtraParametersFunction> methodsWithExtraParametersFunctions = newArrayList();
+
+    public ParametricFunctionBuilder(Class<?> clazz)
+    {
+        this.clazz = clazz;
+    }
+
+    public ParametricFunctionBuilder signature(Signature signature)
+    {
+        this.signature = checkNotNull(signature, "signature is null");
+        return this;
+    }
+
+    public ParametricFunctionBuilder description(String description)
+    {
+        this.description = description;
+        return this;
+    }
+
+    public ParametricFunctionBuilder hidden(boolean hidden)
+    {
+        this.hidden = hidden;
+        return this;
+    }
+
+    public ParametricFunctionBuilder deterministic(boolean deterministic)
+    {
+        this.deterministic = deterministic;
+        return this;
+    }
+
+    public ParametricFunctionBuilder nullableResult(boolean nullableResult)
+    {
+        this.nullableResult = nullableResult;
+        return this;
+    }
+
+    public ParametricFunctionBuilder nullableArguments(boolean... nullableArguments)
+    {
+        checkNotNull(nullableArguments, "nullableArguments is null");
+
+        ImmutableList.Builder<Boolean> nullableArgumentsBuilder = ImmutableList.builder();
+        for (boolean nullableArgument : nullableArguments) {
+            nullableArgumentsBuilder.add(nullableArgument);
+        }
+        this.nullableArguments = nullableArgumentsBuilder.build();
+        return this;
+    }
+
+    public ParametricFunctionBuilder nullableArguments(List<Boolean> nullableArguments)
+    {
+        this.nullableArguments = copyOf(checkNotNull(nullableArguments, "nullableArguments is null"));
+        return this;
+    }
+
+    public ParametricFunctionBuilder methods(String... methodNames)
+    {
+        return methods(asList(checkNotNull(methodNames, "methodNames is null")));
+    }
+
+    public ParametricFunctionBuilder methods(List<String> methodNames)
+    {
+        checkNotNull(methodNames, "methodNames is null");
+
+        List<Method> matchingMethods = asList(clazz.getMethods()).stream()
+                .filter(method -> methodNames.contains(method.getName()))
+                .collect(toList());
+        List<String> matchingMethodNames = matchingMethods.stream()
+                .map(Method::getName)
+                .collect(toList());
+
+        for (String methodName : methodNames) {
+            checkState(matchingMethodNames.contains(methodName), "method %s was not found in class %s", methodName, clazz);
+        }
+
+        methodsWithExtraParametersFunctions.add(new MethodsWithExtraParametersFunction(matchingMethods));
+        return this;
+    }
+
+    public ParametricFunctionBuilder extraParameters(ExtraParametersFunction extraParametersFunction)
+    {
+        checkNotNull(extraParametersFunction, "extraParametersFunction is null");
+        checkState(!methodsWithExtraParametersFunctions.isEmpty(), "no methods are selected (call methods() first)");
+        checkState(!getLast(methodsWithExtraParametersFunctions).getExtraParametersFunction().isPresent(), "extraParameters already defined for selected methods");
+
+        getLast(methodsWithExtraParametersFunctions).setExtraParametersFunction(extraParametersFunction);
+        return this;
+    }
+
+    public ParametricFunction build()
+    {
+        checkState(signature != null, "signature is null");
+
+        if (nullableArguments.isEmpty()) {
+            nullableArguments = Collections.nCopies(signature.getArgumentTypes().size(), false);
+        }
+
+        return new ScalarPolymorphicParametricFunction(signature, description, hidden, deterministic, nullableResult, nullableArguments, methodsWithExtraParametersFunctions);
+    }
+
+    @FunctionalInterface
+    public interface ExtraParametersFunction
+    {
+        List<Object> computeExtraParameters(Map<String, Type> typeMappings, Map<String, Long> literalMappings);
+    }
+
+    static class MethodsWithExtraParametersFunction
+    {
+        private final List<Method> methods;
+        private Optional<ExtraParametersFunction> extraParametersFunction = Optional.empty();
+
+        private MethodsWithExtraParametersFunction(List<Method> methods)
+        {
+            this.methods = methods;
+        }
+
+        List<Method> getMethods()
+        {
+            return methods;
+        }
+
+        Optional<ExtraParametersFunction> getExtraParametersFunction()
+        {
+            return extraParametersFunction;
+        }
+
+        private void setExtraParametersFunction(ExtraParametersFunction extraParametersFunction)
+        {
+            this.extraParametersFunction = Optional.of(extraParametersFunction);
+        }
+    }
+}
---
+++ presto-main/src/main/java/com/facebook/presto/metadata/ScalarPolymorphicParametricFunction.java
@@ -0,0 +1,189 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.metadata;
+
+import com.facebook.presto.metadata.ParametricFunctionBuilder.ExtraParametersFunction;
+import com.facebook.presto.metadata.ParametricFunctionBuilder.MethodsWithExtraParametersFunction;
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.spi.type.TypeManager;
+import com.facebook.presto.spi.type.TypeSignature;
+import com.facebook.presto.util.Reflection;
+import com.google.common.collect.ImmutableMap;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalLong;
+
+import static com.facebook.presto.type.TypeUtils.extractLiteralParameters;
+import static com.facebook.presto.type.TypeUtils.resolveCalculatedType;
+import static com.facebook.presto.type.TypeUtils.resolveType;
+import static com.facebook.presto.type.TypeUtils.resolveTypes;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+import static java.util.Collections.emptyList;
+import static java.util.Locale.US;
+import static java.util.stream.Collectors.toMap;
+
+class ScalarPolymorphicParametricFunction
+        implements ParametricFunction
+{
+    private final Signature signature;
+    private final String description;
+    private final boolean hidden;
+    private final boolean deterministic;
+    private final boolean nullableResult;
+    private final List<Boolean> nullableArguments;
+    private final List<MethodsWithExtraParametersFunction> methodsWithExtraParametersFunctions;
+
+    ScalarPolymorphicParametricFunction(Signature signature, String description, boolean hidden, boolean deterministic,
+            boolean nullableResult, List<Boolean> nullableArguments, List<MethodsWithExtraParametersFunction> methodsWithExtraParametersFunctions)
+    {
+        this.signature = checkNotNull(signature, "signature is null");
+        this.description = description;
+        this.hidden = hidden;
+        this.deterministic = deterministic;
+        this.nullableResult = nullableResult;
+        this.nullableArguments = checkNotNull(nullableArguments, "nullableArguments is null");
+        this.methodsWithExtraParametersFunctions = checkNotNull(methodsWithExtraParametersFunctions, "methodsWithExtraParametersFunctions is null");
+    }
+
+    @Override
+    public Signature getSignature()
+    {
+        return signature;
+    }
+
+    @Override
+    public boolean isScalar()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isAggregate()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isHidden()
+    {
+        return hidden;
+    }
+
+    @Override
+    public boolean isApproximate()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isWindow()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isDeterministic()
+    {
+        return deterministic;
+    }
+
+    @Override
+    public boolean isUnbound()
+    {
+        return false;
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return description;
+    }
+
+    @Override
+    public FunctionInfo specialize(Map<String, Type> types, List<TypeSignature> parameterTypes, TypeManager typeManager, FunctionRegistry functionRegistry)
+    {
+        Map<String, OptionalLong> inputLiteralParameters = signature.bindLiteralParameters(parameterTypes);
+        TypeSignature calculatedReturnType = resolveCalculatedType(signature.getReturnType(), inputLiteralParameters);
+        Map<String, OptionalLong> outputLiteralParameters = extractLiteralParameters(signature.getReturnType(), calculatedReturnType);
+        outputLiteralParameters.entrySet().removeAll(inputLiteralParameters.entrySet());
+        Map<String, OptionalLong> literalParameters = ImmutableMap.<String, OptionalLong>builder()
+                .putAll(inputLiteralParameters)
+                .putAll(outputLiteralParameters)
+                .build();
+
+        List<Type> resolvedParameterTypes = resolveTypes(parameterTypes, typeManager);
+        Type resolvedReturnType = resolveType(calculatedReturnType, typeManager);
+
+        Optional<Method> matchingMethod = Optional.empty();
+        Optional<ExtraParametersFunction> matchingExtraParametersFunction = Optional.empty();
+        for (MethodsWithExtraParametersFunction methodsWithExtraParametersFunction : methodsWithExtraParametersFunctions) {
+            for (Method method : methodsWithExtraParametersFunction.getMethods()) {
+                if (matchesParameterAndReturnTypes(method, resolvedParameterTypes, resolvedReturnType)) {
+                    if (matchingMethod.isPresent()) {
+                        throw new IllegalStateException("two matching methods (" + matchingMethod.get().getName() + " and " + method.getName() + ") for parameter types " + parameterTypes);
+                    }
+                    matchingMethod = Optional.of(method);
+                    matchingExtraParametersFunction = methodsWithExtraParametersFunction.getExtraParametersFunction();
+                }
+            }
+        }
+        checkState(matchingMethod.isPresent(), "no matching method for parameter types %s", parameterTypes);
+
+        List<Object> extraParameters = computeExtraParameters(types, literalParameters, matchingExtraParametersFunction);
+        MethodHandle matchingMethodHandle = applyExtraParameters(matchingMethod, extraParameters);
+
+        return new FunctionInfo(signature, description, hidden, matchingMethodHandle, deterministic, nullableResult, nullableArguments);
+    }
+
+    private boolean matchesParameterAndReturnTypes(Method method, List<Type> resolvedTypes, Type returnType)
+    {
+        checkState(method.getParameterCount() >= resolvedTypes.size(),
+                "method %s has not enough arguments: %s (should have at least %s)", method.getName(), method.getParameterCount(), resolvedTypes.size());
+
+        Class<?>[] methodParameterJavaTypes = method.getParameterTypes();
+        for (int i = 0; i < resolvedTypes.size(); ++i) {
+            if (!methodParameterJavaTypes[i].equals(resolvedTypes.get(i).getJavaType())) {
+                return false;
+            }
+        }
+
+        return method.getReturnType().equals(returnType.getJavaType());
+    }
+
+    private List<Object> computeExtraParameters(Map<String, Type> types, Map<String, OptionalLong> boundLiterals, Optional<ExtraParametersFunction> matchingExtraParametersFunction)
+    {
+        Map<String, Long> filteredBoundLiterals = boundLiterals.entrySet().stream()
+                .filter(entry -> entry.getValue().isPresent())
+                .collect(toMap(entry -> entry.getKey().toLowerCase(US), entry -> entry.getValue().getAsLong()));
+        return matchingExtraParametersFunction.map(function -> function.computeExtraParameters(types, filteredBoundLiterals)).orElse(emptyList());
+    }
+
+    private MethodHandle applyExtraParameters(Optional<Method> matchingMethod, List<Object> extraParameters)
+    {
+        int expectedNumberOfArguments = signature.getArgumentTypes().size() + extraParameters.size();
+        int matchingMethodParameterCount = matchingMethod.get().getParameterCount();
+        checkState(matchingMethodParameterCount == expectedNumberOfArguments,
+                "method %s has invalid number of arguments: %s (should have %s)", matchingMethod.get().getName(), matchingMethodParameterCount, expectedNumberOfArguments);
+
+        MethodHandle matchingMethodHandle = Reflection.methodHandle(matchingMethod.get());
+        matchingMethodHandle = MethodHandles.insertArguments(matchingMethodHandle, signature.getArgumentTypes().size(), extraParameters.toArray());
+        return matchingMethodHandle;
+    }
+}
