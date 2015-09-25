450665b2f55c2cbeb7e9cb8c347fa3e64bdb0617
Christina Wallin <cwallin@hadapt.com>
2015-09-25 16:57:44 -0400

    Update the download links in the documentation to 127t

--- presto-docs/src/main/sphinx/ext/download.py
+++ presto-docs/src/main/sphinx/ext/download.py
@@ -17,9 +17,9 @@ from docutils import nodes, utils
 # noinspection PyUnresolvedReferences
 from sphinx.errors import SphinxError
 
-TD_LINK_SERVER = 'http://teradata-download.s3.amazonaws.com/aster/presto/presto/101t/presto-0.101-1.0.x86_64.rpm'
-TD_LINK_CLI = 'http://teradata-download.s3.amazonaws.com/aster/presto/cli/101t/presto-cli-0.101-executable.jar'
-TD_LINK_JDBC = 'http://teradata-download.s3.amazonaws.com/aster/presto/jdbc/101t/presto-jdbc-0.101.jar'
+TD_LINK_SERVER = 'http://teradata-download.s3.amazonaws.com/aster/presto/presto/127t/presto-0.127-1.0.x86_64.rpm'
+TD_LINK_CLI = 'http://teradata-download.s3.amazonaws.com/aster/presto/cli/127t/presto-cli-0.127-executable.jar'
+TD_LINK_JDBC = 'http://teradata-download.s3.amazonaws.com/aster/presto/jdbc/127t/presto-jdbc-0.127.jar'
 
 GROUP_ID = 'com.facebook.presto'
 ARTIFACTS = {
