e3c33845640399e346229dc13f6d26c60a905b5a
Eric Diven <eric.diven@teradata.com>
2016-05-27 09:42:58 -0400

    Remove redundant test

--- presto-product-tests/src/main/resources/sql-tests/testcases/window_functions/rowsRangeDifferentPartition.sql
+++
@@ -1,9 +0,0 @@
--- database: presto; groups: window;
-select orderkey, suppkey,
-discount,
-round(avg(discount) over (partition by suppkey order by orderkey rows between current row and unbounded following), 5)  avg_discount,
-extendedprice,
-round(avg(extendedprice) over (partition by discount range current row), 5) avg_extendedprice
-
-from tpch.tiny.lineitem where partkey = 272
-
