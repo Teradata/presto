990892dbcbe4187431b61b5226c755615edc2447
Anu Sudarsan <anu.sudarsan@teradata.com>
2016-05-30 12:59:59 -0400

    Add cli changes for ldap authentication
    Add a new "password" cli option which will be the ldap password for the
    user (--user).

--- presto-cli/src/main/java/com/facebook/presto/cli/ClientOptions.java
+++ presto-cli/src/main/java/com/facebook/presto/cli/ClientOptions.java
@@ -75,6 +75,9 @@ public class ClientOptions
     @Option(name = "--user", title = "user", description = "Username")
     public String user = System.getProperty("user.name");
 
+    @Option(name = "--password", title = "password", description = "LDAP password for the user")
+    public String password;
+
     @Option(name = "--source", title = "source", description = "Name of source making query")
     public String source = "presto-cli";
 
@@ -124,6 +127,7 @@ public class ClientOptions
         return new ClientSession(
                 parseServer(server),
                 user,
+                password,
                 source,
                 catalog,
                 schema,
