24a296f29e9139e5b5e340cda6e3420859eba154
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-03-22 13:20:24 +0100

    Test for CostCalculator
    Test queries cover all the current implementation of
    CoefficientCostCalculator.

--- presto-tests/src/test/java/com/facebook/presto/tests/TestTpchDistributedStats.java
+++ presto-tests/src/test/java/com/facebook/presto/tests/TestTpchDistributedStats.java
@@ -13,9 +13,12 @@
  */
 package com.facebook.presto.tests;
 
+import com.facebook.presto.tests.statistics.MetricComparison;
 import org.testng.annotations.Test;
 
+import static com.facebook.presto.tests.statistics.MetricComparison.Result.DIFFER;
 import static com.facebook.presto.tests.statistics.MetricComparison.Result.MATCH;
+import static com.facebook.presto.tests.statistics.MetricComparison.Result.NO_ESTIMATE;
 import static org.testng.Assert.assertEquals;
 
 public class TestTpchDistributedStats
@@ -26,9 +29,110 @@ public class TestTpchDistributedStats
     {}
 
     @Test
-    void testEstimateForSimpleQuery()
+    void testTableScanStats()
     {
-        assertEquals(compareMetric("SELECT * FROM NATION").result(), MATCH);
+        assertEquals(compareMetric("SELECT * FROM nation").result(), MATCH);
+        assertEquals(compareMetric("SELECT * FROM part").result(), MATCH);
+        assertEquals(compareMetric("SELECT * FROM partsupp").result(), MATCH);
+        assertEquals(compareMetric("SELECT * FROM region").result(), MATCH);
+        assertEquals(compareMetric("SELECT * FROM supplier").result(), MATCH);
+        assertEquals(compareMetric("SELECT * FROM lineitem").result(), MATCH);
+        assertEquals(compareMetric("SELECT * FROM lineitem").result(), MATCH);
     }
 
+    @Test
+    void testFilter()
+    {
+        String query = "" +
+                "SELECT * " +
+                "FROM lineitem " +
+                "WHERE l_shipdate <= DATE '1998-12-01' - INTERVAL '90' DAY";
+        MetricComparison metricComparison = compareMetric(query);
+        assertEquals(metricComparison.result(), DIFFER);
+        assertEquals(metricComparison.getEstimate().get(), 30000, 1);
+    }
+
+    @Test
+    void testJoin()
+    {
+        String query = "SELECT * FROM  part, partsupp WHERE p_partkey = ps_partkey";
+        MetricComparison metricComparison = compareMetric(query);
+        assertEquals(metricComparison.result(), DIFFER);
+        assertEquals(metricComparison.getEstimate().get(), 16000, 1);
+    }
+
+    @Test
+    void testUnion()
+    {
+        String query = "SELECT * FROM nation UNION SELECT * FROM nation";
+        MetricComparison metricComparison = compareMetric(query);
+        assertEquals(metricComparison.result(), NO_ESTIMATE);
+    }
+
+    @Test
+    void testIntersect()
+    {
+        String query = "SELECT * FROM nation INTERSECT SELECT * FROM nation";
+        MetricComparison metricComparison = compareMetric(query);
+        assertEquals(metricComparison.result(), NO_ESTIMATE);
+    }
+
+    @Test
+    void testExcept()
+    {
+        String query = "SELECT * FROM nation EXCEPT SELECT * FROM nation";
+        MetricComparison metricComparison = compareMetric(query);
+        assertEquals(metricComparison.result(), NO_ESTIMATE);
+    }
+
+    @Test
+    void testEnforceSingleRow()
+    {
+        String query = "SELECT (SELECT max(n_regionkey) FROM nation)";
+        MetricComparison metricComparison = compareMetric(query);
+        assertEquals(metricComparison.result(), MATCH);
+    }
+
+    @Test
+    void testValues()
+    {
+        String query = "VALUES 1";
+        MetricComparison metricComparison = compareMetric(query);
+        assertEquals(metricComparison.result(), MATCH);
+    }
+
+    @Test
+    void testSemiJoin()
+    {
+        String query = "SELECT * FROM nation WHERE n_regionkey IN (SELECT r_regionkey FROM region)";
+        MetricComparison metricComparison = compareMetric(query);
+        assertEquals(metricComparison.result(), MATCH);
+    }
+
+    @Test
+    void testUselessLimit()
+    {
+        String query = "SELECT * FROM nation LIMIT 30";
+        MetricComparison metricComparison = compareMetric(query);
+        assertEquals(metricComparison.result(), MATCH);
+    }
+
+    @Test
+    void testLimit()
+    {
+        String query = "SELECT * FROM nation LIMIT 10";
+        MetricComparison metricComparison = compareMetric(query);
+        assertEquals(metricComparison.result(), MATCH);
+        assertEquals(metricComparison.getEstimate().get(), 10, 1);
+    }
+
+    @Test
+    void testGroupBy()
+    {
+        String query = "" +
+                "SELECT l_returnflag, l_linestatus " +
+                "FROM lineitem " +
+                "GROUP BY l_returnflag, l_linestatus";
+        assertEquals(compareMetric(query).result(), NO_ESTIMATE);
+    }
 }
